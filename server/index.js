bd0f5f3ea4506318f21560b231cb8857278a8466
K C <110kc3@gmail.com>
2019-10-30 10:45:30 +0100

    About page

--- server/index.js
+++ server/index.js
@@ -1,33 +1,4 @@
-/*var http = require('http');
-var fs = require('fs');
-
-http.createServer(function (req, res) {
-    fs.readFile('index.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'}); // adding http header,  200 is status code and means everything is ok
-    res.write('Version 1.0.0'); // write a response to the client
-    res.write('Hello World!'); // write a response to the client
-    res.end(); // end the response
-    })
-}).listen(8080); // the server object listens on port 8080
-
-*/
-
 /*
-var http = require('http');
-var fs   = require('fs');
-var express = require('express');
-var app = express();
-app.use('/static', express.static('static'))
-
-http.createServer(function (req, res) {
-    fs.readFile('index.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    res.end();
-  });
-}).listen(8080);
-
-
 For node express usage (in project folder - cmd)
 
 npm install express --save
@@ -59,6 +30,6 @@ app.get('/', function(req, resp) {
 
 //Listening on port
 //opening on phone/laptop - 192.168.1.61:3000 - device ipv4 address
-app.listen(3000, '192.168.1.61' || 'localhost', function() {
+app.listen(3000, '157.158.201.193' || 'localhost', function() {
     console.log('Application worker ' + process.pid + ' started...');
 });

24dd4348f1aa4478551ea2c7bd0d18d315e13cd8
jeffbebe <oskarw46@gmail.com>
2019-11-02 02:17:51 +0100

    Added a single button, able to push data into database

--- server/index.js
+++ server/index.js
@@ -10,14 +10,41 @@ npm install --save bootstrap
 
 var express = require('express');
 var path = require('path');
+var mysql = require('mysql');
 var app = express();
-var port = 8080;
+var port = 3000;
 
-//Getiing css file
+// connecting to our database
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "asder12345",
+    database: "mydb"
+  });
+  
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    // that makes our new database
+   /*  con.query("CREATE DATABASE mydb", function (err, result) {    
+        if (err) throw err;
+        console.log("Database created");
+      }); */
+  });
+
+// creating a new table, can be outside the con.connect
+/*   var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  }); */
+
+
+
+
+//Getting css file
 //app.use('/views/css',express.static(__dirname + '/views/css'));
 app.use(express.static(path.join(__dirname, 'views')));
-
-
 
 
 //Getting html file
@@ -25,11 +52,24 @@ app.get('/', function(req, resp) {
     resp.sendFile('index.html', { root: path.join(__dirname, './views') });
 })
 
+//make defined actions after a POST on /control website
+app.post('/control', (req, res) => {
+    var sql = "INSERT INTO requests (Water, Fertilise) VALUES ('1', '1')";
+    con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Records inserted");
+    });
+  })
+
+   
+
+
+
 //bootstrap usage?
 //app.use('/views/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 
 //Listening on port
 //opening on phone/laptop - 192.168.1.61:3000 - device ipv4 address
-app.listen(3000, '157.158.201.193' || 'localhost', function() {
+app.listen(3000, 'localhost', function() {
     console.log('Application worker ' + process.pid + ' started...');
 });

c41bf46e7d7ed42369b8062e49410e3fbdfbe28c
jeffbebe <oskarw46@gmail,com>
2019-11-06 11:18:56 +0100

    added axios, sequelize, posting data script, quoted connection to DB

--- server/index.js
+++ server/index.js
@@ -8,29 +8,29 @@ For bootstrap usage
 npm install --save bootstrap
 */
 
-var express = require('express');
-var path = require('path');
-var mysql = require('mysql');
+var express = require("express");
+var path = require("path");
+var mysql = require("mysql");
 var app = express();
 var port = 3000;
-
+var axios = require("axios");
 // connecting to our database
 var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "asder12345",
-    database: "mydb"
-  });
-  
-  con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-    // that makes our new database
-   /*  con.query("CREATE DATABASE mydb", function (err, result) {    
+  host: "localhost",
+  user: "root",
+  password: "asder12345",
+  database: "mydb"
+});
+
+con.connect(function(err) {
+  if (err) console.log("not Connected!"); //throw err;
+  // console.log("Connected!");
+  // that makes our new database
+  /*  con.query("CREATE DATABASE mydb", function (err, result) {    
         if (err) throw err;
         console.log("Database created");
       }); */
-  });
+});
 
 // creating a new table, can be outside the con.connect
 /*   var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
@@ -39,37 +39,34 @@ var con = mysql.createConnection({
     console.log("Table created");
   }); */
 
-
-
-
 //Getting css file
 //app.use('/views/css',express.static(__dirname + '/views/css'));
-app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, "views")));
 
+// used to parse request body
+app.use(express.json());
 
 //Getting html file
-app.get('/', function(req, resp) {
-    resp.sendFile('index.html', { root: path.join(__dirname, './views') });
-})
+app.get("/", function(req, resp) {
+  resp.sendFile("index.html", { root: path.join(__dirname, "./views") });
+});
 
 //make defined actions after a POST on /control website
-app.post('/control', (req, res) => {
-    var sql = "INSERT INTO requests (Water, Fertilise) VALUES ('1', '1')";
-    con.query(sql, function (err, result) {
+app.post("/control", (req, res) => {
+  let action = req.body.action; // let is sth like var
+  console.log(action);
+  /* var sql = "INSERT INTO requests (Water, Fertilise) VALUES ('1', '1')";
+  con.query(sql, function(err, result) {
     if (err) throw err;
     console.log("Records inserted");
-    });
-  })
-
-   
-
-
+  }); */
+});
 
 //bootstrap usage?
 //app.use('/views/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 
 //Listening on port
 //opening on phone/laptop - 192.168.1.61:3000 - device ipv4 address
-app.listen(3000, 'localhost', function() {
-    console.log('Application worker ' + process.pid + ' started...');
+app.listen(3000, "localhost", function() {
+  console.log("Application worker " + process.pid + " started...");
 });

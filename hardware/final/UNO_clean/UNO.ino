b726ce53d9e111283a1b6f81edd6cdb770ffc441
K C <110kc3@gmail.com>
2019-11-24 12:23:48 +0100

    Add test code to turn on led, read sensor data.

--- hardware/final/UNO_clean/UNO.ino
+++ hardware/final/UNO_clean/UNO.ino
@@ -1,16 +1,14 @@
 
 void setup(){
 
-	Serial.begin(9600);
-	
-
+  pinMode(LED_BUILTIN, OUTPUT);   // initialize digital pin LED_BUILTIN as an output.
+  Serial.begin(9600);
 }
-int incomingByte = 0; // for inco0ming serial data
-
-void loop(){
-
 String serialMessage = "";
 bool stringComplete = false;
+void loop(){
+
+
 while (Serial.available()) // jesli cos ma w buforze
     {
         char serialChar = Serial.read(); // zczytaj znak
@@ -30,22 +28,24 @@ while (Serial.available()) // jesli cos ma w buforze
     }
     if( stringComplete == true)
     {
+      if( serialMessage == "Water")
+      {
+         digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+      }
+      if(serialMessage == "Fertilise")
+      {
+         digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+      }
+      Serial.println("<HelloFromUNO>");
+      delay(1);
+      //Serial.println("<HelloFromUNO>");
       //Serial.print("Received text: " );
       //Serial.println(serialMessage);
       stringComplete = false;
-      Serial.println(serialMessage);
-      //String outMessage = "HelloFromUno "; 
-     // writeString(outMessage);
-      int outInt = 88;
-      //Serial.write(outInt);
-      
-      Serial.println("HelloFromUNO\n");
-      delay(100);
-       Serial.println("HelloFromUNO\n");
+      //Serial.println(serialMessage);
+
         delay(100);
-       Serial.println("HelloFromUNO\n");
-        delay(100);
-       Serial.println("HelloFromUNO\n");
+
     }
   
   
@@ -56,18 +56,18 @@ while (Serial.available()) // jesli cos ma w buforze
 //       Serial.print("Incomming data: ");
 //       Serial.println(val);
 //       if( val == '\n')
-//      	{
+//        {
 //         Serial.println("slash n detected");
 //         val = "";
 //        }
-  delay(500);
+
    }
   
-void writeString(String stringData) { // Used to serially push out a String with Serial.write()
-
-  for (int i = 0; i < stringData.length(); i++)
-  {
-    Serial.write(stringData[i]);   // Push each char 1 by 1 on each loop pass
-  }
-
-}// end writeString
+//void writeString(String stringData) { // Used to serially push out a String with Serial.write()
+//
+//  for (int i = 0; i < stringData.length(); i++)
+//  {
+//    Serial.write(stringData[i]);   // Push each char 1 by 1 on each loop pass
+//  }
+//
+//}// end writeString
